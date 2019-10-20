facfd00389863127196a71e4d4bfb8a412911d93
Nate Jordre <jordre3@gmail.com>
2019-10-20 11:36:48 -0500

    Create Account (Nate & Kate)

--- app/src/main/java/com/example/socialhour/MainActivity.java
+++ app/src/main/java/com/example/socialhour/MainActivity.java
@@ -5,29 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
-
-import com.example.services.importCalendar;
 import android.widget.TextView;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity{
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String nextEvent;
-        try {
-            nextEvent = importCalendar.getNextEvent();
-        }
-        catch(IOException | GeneralSecurityException ex){
-            ex.printStackTrace();
-            nextEvent = "XXX";
-        }
-        TextView eventText= findViewById(R.id.textView5);
-        eventText.setText(nextEvent);
-        System.out.println(nextEvent);
-
+        String name = LogOn.username;
+        name = name.replace("@gmail.com", "");
+        TextView welcomeText = (TextView) findViewById(R.id.textView);
+        welcomeText.setText("Welcome, " + name);
     }
 }
