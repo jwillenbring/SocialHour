9df1778d27fe24d1ef319a8c309ebfdeed91ddbb
kathryncdempsey <kathryncdempsey@gmail.com>
2019-10-23 13:46:52 -0500

    Added a completed Create Group page and empty group home page

---
+++ app/src/main/java/com/example/socialhour/GroupsPage.java
@@ -0,0 +1,14 @@
+package com.example.socialhour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups_page);
+    }
+}
