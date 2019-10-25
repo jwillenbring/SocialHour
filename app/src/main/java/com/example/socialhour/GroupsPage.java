ce95ee232a8fdc51a8626b63ddd5450a8a0a96c1
kathryncdempsey <kathryncdempsey@gmail.com>
2019-10-24 22:00:41 -0500

    Groups page connected to Create a Group

--- app/src/main/java/com/example/socialhour/GroupsPage.java
+++ app/src/main/java/com/example/socialhour/GroupsPage.java
@@ -2,13 +2,29 @@ package com.example.socialhour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GroupsPage extends AppCompatActivity {
+
+    Button createGroup, viewPending;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups_page);
+
+        createGroup = (Button) findViewById(R.id.createGroup);
+
+        createGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),CreateGroup.class));
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/socialhour/MainActivity.java
+++ app/src/main/java/com/example/socialhour/MainActivity.java
@@ -3,10 +3,11 @@ package com.example.socialhour;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,6 +16,8 @@ import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity{
+
+    Button groups, events;
 
     private FirebaseAuth mAuth;
 
@@ -26,5 +29,15 @@ public class MainActivity extends AppCompatActivity{
         name = name.replace("@gmail.com", "");
         TextView welcomeText = (TextView) findViewById(R.id.textView);
         welcomeText.setText("Welcome, " + name);
+
+        groups = (Button) findViewById(R.id.groups);
+        events = (Button) findViewById(R.id.events);
+
+        groups.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),GroupsPage.class));
+            }
+        });
     }
 }
