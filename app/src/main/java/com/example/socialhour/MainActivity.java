9a43f30bd44204f3e3967975458e0f2d5872349d
Nate Jordre <jordre3@gmail.com>
2019-10-24 20:00:32 -0500

    add logout functionality

--- app/src/main/java/com/example/socialhour/MainActivity.java
+++ app/src/main/java/com/example/socialhour/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.socialhour;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 
@@ -17,6 +18,7 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity{
 
     private FirebaseAuth mAuth;
+    Button logOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +28,14 @@ public class MainActivity extends AppCompatActivity{
         name = name.replace("@gmail.com", "");
         TextView welcomeText = (TextView) findViewById(R.id.textView);
         welcomeText.setText("Welcome, " + name);
+        logOutButton = (Button) findViewById(R.id.logOutButton);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LogOn.class));
+            }
+        });
+
+
     }
 }
