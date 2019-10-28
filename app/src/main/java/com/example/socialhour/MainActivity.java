585367e2106f19e232a054deac8378f4bdd60865
jtholen001 <30531217+jtholen001@users.noreply.github.com>
2019-10-27 19:51:32 -0500

    initial google sign in

--- app/src/main/java/com/example/socialhour/MainActivity.java
+++ app/src/main/java/com/example/socialhour/MainActivity.java
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.Task;
+import com.google.api.services.calendar.CalendarScopes;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,6 +27,7 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity{
 
+    private static final int RC_SIGN_IN = 112;
     Button groups, events;
 
     private FirebaseAuth mAuth;
@@ -52,6 +62,49 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestScopes(new Scope(CalendarScopes.CALENDAR_READONLY))
+                .build();
 
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account != null)
+        {
+            System.out.println(account.getEmail());
+        }
+        else {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        {
+            try {
+                GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+                // Signed in successfully, show authenticated UI.
+                System.out.println("successful sign in " + account);
+            } catch (ApiException e) {
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                Log.w("", "signInResult:failed code=" + e.getStatusCode());
+                System.out.println("Something went wrong");
+            }
+        }
     }
 }
